d29cf79bb71f9cbc21a1a9ccfb666febe292feda
George Cheng <Gerhut@GMail.com>
2019-06-10 14:19:48 +0800

    update

--- index.js
+++ index.js
@@ -1,52 +1,26 @@
-const { promises: { stat } } = require('fs')
-const { basename, extname, join, normalize, resolve } = require('path')
-
 const debug = require('debug')('ksp')
 
+const config = require('./lib/config')
+const resolve = require('./lib/resolve')
 const render = require('./lib/render')
 
 /**
- * @param {object} options
- * @param {string} [options.root='.']
- * @param {string} [options.index='index.ksp']
+ * @param {import('./lib/config').KspConfiguration} [conf]
  * @returns {import('koa').Middleware}
  */
-module.exports = ({ root = '.', index = 'index.ksp' } = {}) => {
-  // Normalize arguments
-  root = resolve(root)
-  index = basename(index)
+module.exports = (conf) => {
+  const configuration = config(conf)
+  debug('configuration: %o', configuration)
 
-  debug('root: %s', root)
-  debug('index %s', index)
+  const renderFunction = render(configuration)
 
-  const renderFunction = render({ root })
+  return async (context) => {
+    const pathname = await resolve(context, configuration)
+    debug('pathname: %s', pathname)
 
-  return async (context, next) => {
-    try {
-      let filename = normalize(context.path)
-      filename = join(root, filename)
-
-      let stats = await stat(filename)
-      if (stats.isDirectory()) {
-        debug('%s is directory, appending %s', filename, index)
-        filename = join(filename, index)
-        stats = await stat(filename)
-      }
-
-      const extension = extname(filename)
-      if (extension === '.ksp') {
-        debug('render %s', filename)
-        return await renderFunction(context, filename)
-      }
-    } catch (error) {
-      debug('Error %o', error)
-      const { code } = error
-      context.assert(code !== 'EACCES', 403)
-      context.assert(code !== 'EISDIR', 403)
-      context.assert(code !== 'ENOENT', 404)
-      context.assert(code !== 'EMFILE', 503)
-      throw error
+    const renderResult = await renderFunction(context, pathname)
+    if (context.body === undefined) {
+      context.body = renderResult
     }
-    return next()
   }
 }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 
-const path = require('path')
+const { resolve } = require('path')
 
 const should = require('should')
 const axiosist = require('axiosist')
@@ -9,38 +9,44 @@ const Koa = require('koa')
 const ksp = require('..')
 
 class KspApp extends Koa {
-  constructor (options) {
+  /**
+   * @param {import('../lib/config').KspConfiguration} [conf]
+   */
+  constructor (conf) {
     super()
-    const controller = ksp(options)
+    const controller = ksp(conf)
     this.use(controller)
   }
 }
 
+const root = resolve(__dirname, 'fixtures')
+
 it('should works', async () => {
-  const app = new KspApp({ root: path.resolve(__dirname, 'fixtures') })
+  const app = new KspApp({ root })
   const response = await axiosist(app.callback()).get('/hello.ksp')
   should(response.data).be.startWith('Hello world')
 })
 
 it('should 404', async () => {
-  const app = new KspApp({ root: path.resolve(__dirname, 'fixtures') })
-  try {
-    await axiosist(app.callback()).get('/hello2.ksp')
-    should.fail()
-  } catch (error) {
-    console.log(error)
-    should(error.response.status).be.equal(404)
-  }
+  const app = new KspApp({ root })
+  const response = await axiosist(app.callback()).get('/hello2.ksp')
+  response.status.should.be.equal(404)
 })
 
 it('should index', async () => {
-  const app = new KspApp({ root: path.resolve(__dirname, 'fixtures') })
+  const app = new KspApp({ root })
   const response = await axiosist(app.callback()).get('/')
   should(response.data).be.startWith('Hello index')
 })
 
+it('should not support nested index', async () => {
+  const app = new KspApp({ root })
+  const response = await axiosist(app.callback()).get('/hello')
+  response.status.should.be.equal(404)
+})
+
 it('should support returned template', async () => {
-  const app = new KspApp({ root: path.resolve(__dirname, 'fixtures') })
+  const app = new KspApp({ root })
   const response = await axiosist(app.callback()).get('/return.ksp')
   should(response.data).be.equal('returned')
 })
